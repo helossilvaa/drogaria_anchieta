bacd28e6658cf4aef229d8d83d2406f5de6ed465
Beatriz <beatrizrobertadarruda@gmail.com>
2025-11-05 16:36:31 -0300

    Resolvendo conflitos

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,6 @@ import descontosRotas from "./routes/descontosRotas.js";
 import fornecedoresRotas from './routes/fornecedoresRoutes.js';
 import contasFilialRotas from './routes/contasFilialRotas.js';
 import produtosRotas from './routes/produtosRotas.js';
-import salariosRotas from './routes/salariosRotas.js';
-
 
 dotenv.config();
 
@@ -62,7 +60,6 @@ app.use("/api", descontosRotas);
 app.use('/api', fornecedoresRotas);
 app.use('/api', contasFilialRotas);
 app.use ('/produtos', produtosRotas);
-app.use('/api', salariosRotas);
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'online' });
