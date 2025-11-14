3461975fb3ec2d7c0331bb9549017f55f53c6402
Beatriz <beatrizrobertadarruda@gmail.com>
2025-11-14 14:55:07 -0300

    Atualizando

--- frontend/src/app/pdv/filiados/page.jsx
+++ frontend/src/app/pdv/filiados/page.jsx
@@ -23,11 +23,12 @@ export default function Filiados() {
     logradouro: "",
     numero: "",
     tipodesconto: "",
+    unidade_id: "", 
   });
 
   const API_URL = "http://localhost:8080/api/filiados";
 
-  // Buscar usuários e tipos de desconto
+  // Buscar usuários e tipos de desconto 
   useEffect(() => {
     fetchUsuarios();
     fetchTiposDescontos();
@@ -39,6 +40,7 @@ export default function Filiados() {
       const res = await fetch(API_URL, {
         headers: { Authorization: `Bearer ${token}` },
       });
+
       const data = await res.json();
       setUsuarios(data);
     } catch (error) {
@@ -59,7 +61,7 @@ export default function Filiados() {
     }
   };
 
-  // Paginação
+  // Paginação 
   const indexUltimo = paginaAtual * itensPorPagina;
   const indexPrimeiro = indexUltimo - itensPorPagina;
   const usuariosFiltrados = usuarios.filter((u) =>
@@ -67,27 +69,23 @@ export default function Filiados() {
   );
   const usuariosPagina = usuariosFiltrados.slice(indexPrimeiro, indexUltimo);
   const totalPaginas = Math.ceil(usuariosFiltrados.length / itensPorPagina);
-
   const mudarPagina = (numero) => {
     if (numero < 1) numero = 1;
     if (numero > totalPaginas) numero = totalPaginas;
     setPaginaAtual(numero);
   };
 
-  // Atualizar estado e buscar CEP
+  // Atualizar estado e buscar CEP 
   const handleChange = async (e) => {
     const { name, value } = e.target;
-
     setNovoUsuario((prev) => ({
       ...prev,
       [name]: name === "estado" ? value.toUpperCase() : value,
     }));
-
     if (name === "cep" && value.length === 8) {
       try {
         const res = await fetch(`https://viacep.com.br/ws/${value}/json/`);
         const data = await res.json();
-
         if (!data.erro) {
           setNovoUsuario((prev) => ({
             ...prev,
@@ -114,27 +112,25 @@ export default function Filiados() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // Validação de CPF
+    // Validação de CPF 
     if (novoUsuario.cpf.length !== 11) {
       alert("CPF deve conter exatamente 11 dígitos.");
       return;
     }
 
-    // Validação de telefone — deve ter 8 ou 9 dígitos numéricos
+    // Validação de telefone — deve ter 8 ou 9 dígitos numéricos 
     const telefoneNumerico = novoUsuario.telefone.replace(/\D/g, "");
     if (telefoneNumerico.length !== 8 && telefoneNumerico.length !== 9) {
       alert("Telefone deve conter exatamente 8 ou 9 dígitos numéricos.");
       return;
     }
 
-    // Verificar duplicidade de CPF
+    // Verificar duplicidade de CPF 
     const existe = usuarios.find((u) => u.cpf === novoUsuario.cpf);
     if (existe) {
       alert("Usuário com este CPF já está cadastrado.");
       return;
     }
-
     try {
       const token = localStorage.getItem("token");
       const res = await fetch(API_URL, {
@@ -145,7 +141,6 @@ export default function Filiados() {
         },
         body: JSON.stringify(novoUsuario),
       });
-
       if (!res.ok) {
         let erroMsg = "Erro ao salvar usuário";
         try {
@@ -157,8 +152,8 @@ export default function Filiados() {
         }
         throw new Error(erroMsg);
       }
-
       alert("Usuário cadastrado com sucesso!");
+
       setAbrirModal(false);
       setNovoUsuario({
         nome: "",
@@ -191,24 +186,9 @@ export default function Filiados() {
         <button
           type="button"
           onClick={() => setAbrirModal(true)}
-          className="cursor-pointer border p-2 rounded-md bg-blue-500 text-white mt-2"
-        >
-          <svg
-            className="w-6 h-6 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h14m-7 7V5"
-            />
+          className="cursor-pointer border p-2 rounded-md bg-blue-500 text-white mt-2">
+          <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5"/>
           </svg>{" "}
           Novo Usuário
         </button>
@@ -219,12 +199,10 @@ export default function Filiados() {
             placeholder="Buscar usuário por CPF..."
             value={buscaCPF}
             onChange={(e) => setBuscaCPF(e.target.value)}
-            className="border rounded-md p-2 w-64"
-          />
+            className="border rounded-md p-2 w-64"/>
           <button
             onClick={() => setPaginaAtual(1)}
-            className="bg-gray-200 px-3 py-2 rounded hover:bg-gray-300"
-          >
+            className="bg-gray-200 px-3 py-2 rounded hover:bg-gray-300">
             Pesquisar
           </button>
         </div>
@@ -260,7 +238,6 @@ export default function Filiados() {
                     />
                   </div>
                 ))}
-
                 <div>
                   <label>Tipo de Desconto</label>
                   <select
@@ -270,29 +247,29 @@ export default function Filiados() {
                     className="border rounded-md p-2 w-full"
                     required
                   >
+
                     <option value="">Selecione um tipo</option>
+
                     {tiposDescontos.map((t) => (
                       <option key={t.id} value={t.id}>
                         {t.tipo}
                       </option>
                     ))}
+
                   </select>
                 </div>
 
                 <div className="flex justify-between mt-4">
                   <button
                     type="submit"
-                    className="bg-green-600 text-white rounded-md p-2"
-                  >
+                    className="bg-green-600 text-white rounded-md p-2">
                     Salvar
                   </button>
                 </div>
               </form>
-
               <button
                 onClick={() => setAbrirModal(false)}
-                className="absolute top-2 right-3 text-gray-500 hover:text-red-500 text-xl"
-              >
+                className="absolute top-2 right-3 text-gray-500 hover:text-red-500 text-xl">
                 &times;
               </button>
             </div>
@@ -324,6 +301,7 @@ export default function Filiados() {
                   </tr>
                 </thead>
                 <tbody>
+
                   {usuariosPagina.map((u) => (
                     <tr key={u.id} className="border-t">
                       <td className="p-2">{u.nome}</td>
@@ -351,8 +329,7 @@ export default function Filiados() {
                 <button
                   onClick={() => mudarPagina(paginaAtual - 1)}
                   disabled={paginaAtual === 1}
-                  className="px-3 py-1 border rounded disabled:opacity-50 cursor-pointer"
-                >
+                  className="px-3 py-1 border rounded disabled:opacity-50 cursor-pointer">
                   &lt; Anterior
                 </button>
 
@@ -362,9 +339,8 @@ export default function Filiados() {
                     <button
                       key={numeroPagina}
                       onClick={() => mudarPagina(numeroPagina)}
-                      className={`px-3 py-1 border rounded ${
-                        paginaAtual === numeroPagina ? "bg-red-300" : ""
-                      }`}
+                      className={`px-3 py-1 border rounded ${paginaAtual === numeroPagina ? "bg-red-300" : ""
+                        }`}
                     >
                       {numeroPagina}
                     </button>
@@ -374,14 +350,14 @@ export default function Filiados() {
                 <button
                   onClick={() => mudarPagina(paginaAtual + 1)}
                   disabled={paginaAtual === totalPaginas}
-                  className="px-3 py-1 border rounded disabled:opacity-50 cursor-pointer"
-                >
+                  className="px-3 py-1 border rounded disabled:opacity-50 cursor-pointer">
                   Próxima &gt;
                 </button>
               </div>
             )}
           </>
         ) : (
+
           <p className="text-center text-gray-500 mt-6 text-lg">
             Nenhum usuário encontrado para o CPF informado.
           </p>
@@ -389,4 +365,5 @@ export default function Filiados() {
       </Layout>
     </>
   );
-}
+
+} 
