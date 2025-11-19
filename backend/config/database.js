40c8b5132b34ab490034dd9946f6a555650487b6
Mariana <ma.oliveira200508@gmail.com>
2025-11-19 16:33:52 -0300

    produtos e ajustes

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 
 const pool = mysql.createPool({
     host: '10.189.80.99',
-    user: 'Beatriz',
+    user: 'mariana',
     database: 'drogaria',
     password: 'Anchieta@123',
     waitForConnections: true,
